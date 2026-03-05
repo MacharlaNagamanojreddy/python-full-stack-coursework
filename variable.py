f4e8ffbde5e8bcaf434ab6f209b32761563244be
Macharla Naga Manoj Reddy <119473440+MacharlaNagamanojreddy@users.noreply.github.com>
2026-03-05 17:38:22 +0530

    Initial commit: add coursework files

---
+++ variable.py
@@ -0,0 +1,20 @@
+a = 10
+if a%2 == 0:
+    print("a is an even number")
+else:
+    print("a is an odd number")
+    print(type(a))
+b = 3.14
+print(type(b))
+c = "Hello, World!"
+print(type(c))
+d = True
+print(type(d))
+e = [1, 2, 3, 4, 5]
+print(type(e))
+f = (1, 2, 3, 4, 5)
+print(type(f))
+g = {"name": "Alice", "age": 30, "city": "New York"}
+print(type(g))
+h = {1, 2, 3, 4, 5}
+print(type(h)) 
